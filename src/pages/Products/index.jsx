cd03a82bf312039d921911f432703d763d1b8031
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-06-07 09:01:08 +0300

    1

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -22,12 +22,25 @@ function Products() {
           </div>
         )}
         {error.status && <p className="text-red-500">{error.message}</p>}
-        {!isPending && error.message && (
+        {!isPending && !error.status && (
           <div className="mt-10">
             {data.map((product) => (
               <div key={product.id} className="product">
-                <h2>{product.name}</h2>
-                <p>{product.description}</p>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                  <figure>
+                    <img
+                      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                      alt="Shoes"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
