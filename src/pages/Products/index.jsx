8d1317ee9a8d1124976f60da3d4c0621921bc889
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-06-10 12:54:54 +0300

    1

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -13,7 +13,7 @@ function Products() {
       <div className="container">
         <div
           className="flex justify-between items-center
-        pt-5  pb-3 border-b-4"
+             pt-5  pb-3 border-b-4"
         >
           <h1>Products</h1>
           <button
@@ -35,17 +35,21 @@ function Products() {
         )}
         {error.status && <p className="text-red-500">{error.message}</p>}
         {!isPending && !error.status && (
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-24  xl:gap-x-48 mt-10 pb-20">
+          <div className="grid gap-14 grid-cols-1 md:grid-cols-2 xl:grid-cols-3  md:gap-24 xl:gap-x-48 mt-10 pb-20 strech">
             {data.map(
               ({ id, title, description, image, price, raiting, stock }) => (
-                <div key={id} className="product justify-self-center">
-                  <div className="card w-[320px] bg-base-100 shadow-md shadow-primary">
+                <div
+                  key={id}
+                  className="product flex justify-self-center items-stretch"
+                >
+                  <div className="card w-[320px] bg-base-100 shadow-[0_3px_10px_0] shadow-primary">
                     <figure>
                       <img src={image} alt="" width={250} />
                     </figure>
                     <div className="card-body">
-                      <h2 className="card-title">{title}</h2>
-                      <p className="text-lg">{price}$</p>
+                      <h2 className="card-title">Title: {title}</h2>
+                      <p className="text-lg">Price: {price}$</p>
+                      <p>Stock: {stock}</p>
                       <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                       </div>
